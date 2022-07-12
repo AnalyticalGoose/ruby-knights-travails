b401c4683797ee81c377b20fa2a8867ec67ba0ff
Harry Edgar <harryedgar@hotmail.co.uk>
2022-07-12 21:06:35 +0100

    Basic functionality complete, very buggy

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,14 @@
+require 'pry'
+
 require_relative 'variables.rb'
 include Variables
 
 knight = [1, 1]
 target = [4, 7]
 
+#3,5 6,2
 
-
+@solution = "#{knight} -> "
 
 
 # Calculate the X,Y offset from the knight to its target
@@ -22,18 +25,35 @@ def calc_moves(start, moves = [])
        x += 1
        test_move.any? { |n| n <= 0 || n >= 8} ? next : moves << test_move
     end
+    moves
 end
 
 def knight_search(min_moves, moves)
-    depth = CALC_STEPS[min_moves][0]
+     CALC_STEPS[min_moves][0].times do |x = 0|
+        moves.size.times do |y = 0|
+            moves[y] << calc_moves(moves[y])
+            y += 1
+        end   
+        x+=1
+    end
+    moves
 end
 
-def target_search
-
+def find_common_cell(moves, t_moves)
+    moves.size.times do |z = 0|
+        t_moves.size.times do |x = 0|
+            if (t_moves & moves[x][2]).one?
+                @solution.concat("[#{moves[x][0]}, #{moves[x][1]}] -> ")
+                return t_moves & moves[x][2]
+            end
+            x += 1
+        end
+        z += 1
+    end
 end
 
-calc_moves(knight, moves)
-p moves
+moves = knight_search(min_moves, calc_moves(knight, moves))
+t_moves = calc_moves(target, t_moves).map { |i| i }
+@solution.concat("#{find_common_cell(moves, t_moves)} -> #{target}")
 
-calc_moves(target, t_moves)
-p t_moves
+puts @solution
